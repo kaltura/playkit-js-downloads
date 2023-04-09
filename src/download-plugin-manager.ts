33db2961a61785bb0c08327bb69d472d4a60c711
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-04-09 16:11:34 +0300

    add pause/play on overlay toggle

--- src/download-plugin-manager.ts
+++ src/download-plugin-manager.ts
@@ -2,11 +2,12 @@ import { DownloadService } from "services";
 import { DownloadConfig, DownloadMetadata } from "types";
 
 class DownloadPluginManager extends KalturaPlayer.core.FakeEventTarget {    
-    private _showOverlay: boolean = false;
+    private _showOverlay = false;
     private downloadService: any;
     private downloadMetadata: DownloadMetadata = null;
+    private playOnClose = false;
 
-    constructor(player: KalturaPlayerTypes.Player, private config: DownloadConfig) {
+    constructor(private player: KalturaPlayerTypes.Player, private config: DownloadConfig) {
         super();
         this.downloadService = new DownloadService(player);
     }
@@ -22,8 +23,16 @@ class DownloadPluginManager extends KalturaPlayer.core.FakeEventTarget {
         this._showOverlay = overlayVisible;
 
         if (this._showOverlay) {
+            if (!this.player.paused) {
+                this.player.pause();
+                this.playOnClose = true;
+            }
             this.dispatchEvent(new KalturaPlayer.core.FakeEvent("showoverlay"));
         } else {
+            if (this.playOnClose) {
+                this.player.play();
+                this.playOnClose = false;
+            }
             this.dispatchEvent(new KalturaPlayer.core.FakeEvent("hideoverlay"));
         }
     }
