1355c455b20cc0d6b5a33734461a147e3a0b98ed
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-04-09 15:28:30 +0300

    add responsive support

--- src/components/download-overlay/download-overlay.tsx
+++ src/components/download-overlay/download-overlay.tsx
@@ -1,18 +1,47 @@
-import {Button, OnClickEvent, OnClick, ButtonType, ButtonSize} from '@playkit-js/common';
+import {Button, ButtonType, ButtonSize} from '@playkit-js/common';
 import { DownloadPluginManager } from 'download-plugin-manager';
 
 import {useState, useEffect} from 'preact/hooks';
 import { DownloadMetadata } from 'types';
-const {withEventManager} = KalturaPlayer.ui.Event;
 
-const {Icon, IconType} = KalturaPlayer.ui.components;
+const {withEventManager} = KalturaPlayer.ui.Event;
+const {Icon, IconType, PLAYER_SIZE} = KalturaPlayer.ui.components;
+const {connect} = KalturaPlayer.ui.redux;
 
 import * as styles from './download-overlay.scss';
 
 interface DownloadOverlayProps {
     downloadPluginManager: DownloadPluginManager;
     eventManager: any;
+    sizeClass?: string;
 }
+
+const mapStateToProps = (state: any) => {
+    const {shell: {playerSize}} = state;
+    let sizeClass = "";
+
+    switch(playerSize) {
+        case PLAYER_SIZE.EXTRA_LARGE:
+        case PLAYER_SIZE.LARGE: {
+            sizeClass = styles.large;
+            break;
+        } case PLAYER_SIZE.MEDIUM: {
+            sizeClass = styles.medium;
+            break;
+        }
+        case PLAYER_SIZE.SMALL:
+        case PLAYER_SIZE.EXTRA_SMALL: {
+            sizeClass = styles.small;
+            break;
+        } default: {
+            break;
+        }
+    }
+
+    return {
+      sizeClass
+    };
+  };
 
 const downloadFile = (downloadUrl: string) => {
     const aElement = document.createElement('a');
@@ -21,7 +50,7 @@ const downloadFile = (downloadUrl: string) => {
     aElement.click();
 }
 
-const DownloadOverlay = withEventManager(({downloadPluginManager, eventManager} : DownloadOverlayProps) => {
+const DownloadOverlay = connect(mapStateToProps)(withEventManager(({downloadPluginManager, eventManager, sizeClass} : DownloadOverlayProps) => {
     const [isVisible, setIsVisible] = useState(false);
     const [fileName, setFileName] = useState("");
     const [downloadUrl, setDownloadUrl] = useState("");
@@ -43,20 +72,22 @@ const DownloadOverlay = withEventManager(({downloadPluginManager, eventManager} 
         });
       }, []);
 
+    const TEMP_TEXT = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
     return isVisible ? (
         <div className={styles.downloadOverlay}>
             <div className={styles.header}>Downloads</div>
             <div className={styles.fileInfoList}>
-                <div className={styles.fileInfo}>
-                    <div className={`${styles.iconContainer} ${styles.playIconContainer}`}>
+                <div className={`${styles.fileInfo} ${sizeClass}`}>
+                    <div className={`${styles.iconContainer} ${styles.playIcon}`}>
                         <Icon id="download-file-play" type={IconType.Play} viewBox={`0 0 32 32`}></Icon>
                     </div>
                     <div className={styles.fileInfoTextContainer}>
                         <div className={styles.fileInfoText}>
-                            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                            {TEMP_TEXT}
                         </div>
                     </div>
-                    <div className={styles.iconContainer}>
+                    <div className={`${styles.iconContainer} ${styles.downloadIcon}`}>
                         <div>
                             <Button
                                 type={ButtonType.borderless}
@@ -88,6 +119,6 @@ const DownloadOverlay = withEventManager(({downloadPluginManager, eventManager} 
             </div>
         </div>
         ) : <div></div>
-});
+}));
 
 export {DownloadOverlay};
