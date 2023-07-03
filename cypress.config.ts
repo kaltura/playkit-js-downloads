17f0ae5d23a07e6fd143ff9ef0714e28fa45c012
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-07-03 14:43:46 +0300

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from 'cypress';
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
   fileServerFolder: 'cypress/public',
   e2e: {
     watchForFileChanges: false
