0d5ef42a684171a019bd13cd7dc54230843ae892
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-05-22 18:00:43 +0300

    bring back ts ignore

--- src/download.tsx
+++ src/download.tsx
@@ -103,7 +103,7 @@ class Download extends KalturaPlayer.core.BasePlugin {
           return <DownloadOverlayButton setRef={this._setPluginButtonRef} />;
         },
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error - TS2322: Type string[] is not assignable to type ReservedPresetName[]
+        // @ts-ignore
         presets: PRESETS.filter(presetName => presetName !== ReservedPresetNames.MiniAudioUI)
       }) as number;
     }
