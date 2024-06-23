3081682e9e330e0b381a03a0506a783e637c00df
JonathanTGold <jonathan.gold@86@gmail.com>
2024-06-23 13:04:01 +0300

    fix lint

--- src/providers/response-types/kaltura-flavor-asset-list-response.ts
+++ src/providers/response-types/kaltura-flavor-asset-list-response.ts
@@ -18,10 +18,9 @@ export class KalturaFlavorAssetListResponse extends BaseServiceResult {
   private setDataWithoutDuplicates(flavors: Array<any>): Array<KalturaFlavorAsset> {
     const data = [];
     // sort flavors by height and filter out flavors with height 0
-    const sortedFlavors = flavors
-      .sort((a: {height: number}, b: {height: number}) => {
-        return a.height < b.height ? 1 : -1;
-      })
+    const sortedFlavors = flavors.sort((a: {height: number}, b: {height: number}) => {
+      return a.height < b.height ? 1 : -1;
+    });
     data.push(new KalturaFlavorAsset(sortedFlavors[0]));
     let previousFlavor = data[0];
 
