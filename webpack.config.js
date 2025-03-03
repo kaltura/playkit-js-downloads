527d32bcc989c599995d04180f5f8c2c6d0736a7
lianbenjamin <lian.binyamin@kaltura.com>
2025-03-03 14:30:22 +0200

    feat(FEC-14380): add nonce to style tags

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
+const {insertStylesWithNonce} = require('@playkit-js/webpack-common');
 
 const plugins = [
   new webpack.DefinePlugin({
@@ -59,7 +60,8 @@ module.exports = {
             loader: 'style-loader',
             options: {
               attributes: {id: `${packageData.name}`},
-              injectType: 'singletonStyleTag'
+              injectType: 'singletonStyleTag',
+              insert: insertStylesWithNonce
             }
           },
           {
